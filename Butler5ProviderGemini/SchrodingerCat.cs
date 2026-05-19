ca54cbfa79e480d8084f65bf85ef1075ffef0acc
Thomas Paul Betterly <thomasbetterly@aol.com>
2026-05-19 12:47:06 -0400

    work on the VertexAI support

--- Butler5ProviderGemini/SchrodingerCat.cs
+++ Butler5ProviderGemini/SchrodingerCat.cs
@@ -6,14 +6,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using ButlerSDK.Providers.Gemini;
 namespace ButlerSDK.Provider.Gemini
 {
-    /*
-     * GeminiProvider uses this to keep calls to gemini GoogleAI vs VertexAI on the same code path. It is internal for a reason markeed and can change between releases as the provider changes.
-     */ 
+
+    /// <summary>
+    /// Internal adapter interface that allows <see cref="ButlerGeminiProvider"/>
+    /// to share a single implementation path between AI Studio and Vertex AI backends.
+    /// </summary>
+    /// <remarks>Important: Do not depend on this to be the same between Gemini Provider builds for butler. This interface intentionally not exposed outside the the Gemini Provider boundary.</remarks>
     internal interface SchrodingerCat
     {
+        /*This interface exists purely to unify Butler Gemini Provider  backend SDK differences so that all translation,
+         * tool handling, and streaming logic is 
+         * implemented once and shared across AI Studio and Vertex AI.*/
         GenerativeModel CreateGenerativeModel(string modelName, GenerationConfig? config = null, ICollection<SafetySetting>? safetyRatings = null, string? systemInstruction = null, string? corpusIdForRag = null, RagRetrievalConfig? ragRetrievalConfig = null);
         public Task<ListModelsResponse> ListModelsAsync(int? pageSize = null, string? pageToken = null, CancellationToken cancellationToken = default(CancellationToken));
     }
